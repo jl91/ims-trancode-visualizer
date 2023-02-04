e7c500ac1e66512fed77105b793a5504e03ba726
John Fernandes <jlfernandes91@hotmail.com>
2023-02-04 00:43:22 -0300

    adding basic structure for dynamic form for trancode fields mapping

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -9,24 +9,44 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+
 import { TrancodeMapperComponent } from './trancode-mapper/trancode-mapper.component';
 import { TrancodeViewerComponent } from './trancode-viewer/trancode-viewer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 const material_modules = [
   MatSidenavModule,
   MatDividerModule,
   MatButtonModule,
   MatIconModule,
+  MatTableModule,
+  MatFormFieldModule,
+  MatInputModule
 ];
 
 @NgModule({
-  declarations: [LayoutComponent, HomeComponent, TrancodeMapperComponent, TrancodeViewerComponent],
+  declarations: [
+    LayoutComponent,
+    HomeComponent,
+    TrancodeMapperComponent,
+    TrancodeViewerComponent,
+  ],
   imports: [
     CommonModule,
     RouterModule,
     PagesRoutingModule,
+    ReactiveFormsModule,
     ...material_modules,
   ],
   exports: [PagesRoutingModule],
+  providers:[
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: {appearance: 'outline'}
+    }
+  ]
 })
 export class PagesModule {}
--- src/app/pages/trancode-mapper/trancode-mapper.component.ts
+++ src/app/pages/trancode-mapper/trancode-mapper.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-trancode-mapper',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./trancode-mapper.component.scss']
 })
 export class TrancodeMapperComponent {
+
+  public formGroup: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder
+  ) {
+    this.formGroup = this.formBuilder.group({
+      trancodeName: ['', ''],
+      trancodeFields: this.formBuilder.array([])
+    });
+  }
+
+  public displayedColumns: string[] = [
+    'test'
+  ];
+
+  public data: {
+      columnName: string,
+      columnValue: string
+  }[] = [
+    {
+      columnName: 'test',
+      columnValue: 'teste'
+    }
+  ];
+
+  get trancodeFields(): FormArray {
+    return this.formGroup.controls['trancodeFields'] as FormArray;
+  }
+
+  addTrancodeField(event: MouseEvent): void{
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    event.stopPropagation();
+
+    const newForm = this.formBuilder.group({
+      fieldName: ['', ''],
+      fieldSize: ['biginteger', '']
+    });
+
+    this.trancodeFields.push(newForm);
+  }
 
 }
